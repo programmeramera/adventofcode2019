bce0885561f4f6bc60fcee1e761250038adbb389
Johan Lindfors <Johan.Lindfors@coderox.se>
2019-12-10 06:26:20 +0100

    Now reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    // --snip--
+    let filename = "input/input1_1.txt";
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
