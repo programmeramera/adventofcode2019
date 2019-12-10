e365fd0b4e8b30e345f48c9774e7144976c527f9
Johan Lindfors <johan.lindfors@coderox.se>
2019-12-10 15:46:26 +0100

    Proper tests for day 2 part 1

--- src/day_two.rs
+++ src/day_two.rs
@@ -34,14 +34,14 @@ mod tests {
 
     #[test]
     fn day2_1_test_cases() {
-        assert_eq!(process("1,0,0,0,99"), "1,0,0,0,99");
-        assert_eq!(process("2,3,0,3,99"), "2,3,0,6,99");
-        assert_eq!(process("2,4,4,5,99,0"), "2,4,4,5,99,9801");
+        assert_eq!(process("1,0,0,0,99"), "2,0,0,0,99"); // 1 + 1 = 2
+        assert_eq!(process("2,3,0,3,99"), "2,3,0,6,99"); // 3 * 2 = 6
+        assert_eq!(process("2,4,4,5,99,0"), "2,4,4,5,99,9801"); // 99 * 99 = 9801
         assert_eq!(process("1,1,1,4,99,5,6,0,99"), "30,1,1,4,2,5,6,0,99");
     }
 
-    #[test]
-    fn day2_2_test_cases() {
+    // #[test]
+    // fn day2_2_test_cases() {
 
-    }
+    // }
 }
