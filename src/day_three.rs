4c091f95b6ba1ecc25766236b38c026327ea8e79
Johan Lindfors <johan.lindfors@coderox.se>
2019-12-11 11:33:50 +0100

    Now with failing test for day 3 part 1

--- src/day_three.rs
+++ src/day_three.rs
@@ -16,16 +16,26 @@ fn part_two(_input: &str) {
 
 }
 
+fn manhattan_distance(_input: &str) -> i32 {
+    0
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
-//    #[test]
+    #[test]
     fn day3_1_test_cases() {
-        // assert_eq!(process("1,0,0,0,99",0,0), 2); // 1 + 1 = 2
-        // assert_eq!(process("2,3,0,3,99",3,0), 2); // 3 * 2 = 6
-        // assert_eq!(process("2,4,4,5,99,0",4,4), 2); // 99 * 99 = 9801
-        // assert_eq!(process("1,1,1,4,99,5,6,0,99",1,1), 30);
+        assert_eq!(
+            manhattan_distance(
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72
+                U62,R66,U55,R34,D71,R55,D58,R83")
+            , 159);
+        assert_eq!(
+            manhattan_distance(
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51
+                U98,R91,D20,R16,D67,R40,U7,R15,U6,R7")
+            , 135);
     }
 
 //    #[test]
